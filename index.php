3f3434a03c3483486d3fd58bed942bbe3bfeff78
Ilya <i.zinoviev@fim.ltd>
2021-04-03 11:34:26 +0300

    newvetka

---
+++ index.php
@@ -0,0 +1,19 @@
+ <?php
+
+$url="https://yandex.ru";
+
+//$res=file_get_contents($url);
+
+//print_r($res);
+
+$curl=curl_init();
+curl_setopt($curl,CURLOPT_URL,$url);
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+$res=curl_exec($curl);
+
+//print_r($res);
+echo 'hi';
+preg_match('#<title>(.*?)</title>#su',$res,$matches);
+$title=$matches[1];
+print_r($matches);
+?>
